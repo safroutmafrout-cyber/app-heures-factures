753e2c1b901debd12e8a977ced2e863322a6b67a
zairi <zairi@users.noreply.github.com>
2026-03-15 02:55:21 -0400

    Fix: change drive.file to drive scope - drive.file only sees files created by same OAuth client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,7 +8,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          scope: 'openid email profile https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/gmail.send',
+          scope: 'openid email profile https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/gmail.send',
           access_type: 'offline',
           prompt: 'consent',
         },
